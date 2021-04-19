e5de98f2a424e629ec3a04feeee55e65f33088dd
Renuka Paturkar <67357703+renukapaturkar@users.noreply.github.com>
2021-04-19 18:46:04 +0530

    update: DB connection url

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 function initializeDBconnection() {
-    mongoose.connect("mongodb+srv://renukapaturkar:Eloquence@3497@neog-cluster.fns0z.mongodb.net/Ecommerce?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true})
+    mongoose.connect("mongodb+srv://renukapaturkar:Eloquence@3497@neog-cluster.fns0z.mongodb.net/inventory?retryWrites=true&w=majority", {useUnifiedTopology: true, useNewUrlParser: true})
     .then(() => console.log("mongodb successfully connected"))
     .catch(error => console.error("mongodb connection failed... ", error))
 }
