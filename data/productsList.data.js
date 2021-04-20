e17082a38db0ce5a0caccd17648d5b97f6e7e0f4
Renuka Paturkar <67357703+renukapaturkar@users.noreply.github.com>
2021-04-20 10:55:23 +0530

    fix

--- data/productsList.data.js
+++ data/productsList.data.js
@@ -2,9 +2,9 @@ const productsList = [
 
     {
         "title": "The Alchemist",
-        "image": "https://rukminim1.flixcart.com/image/612/612/kjhgzgw0-0/book/m/m/u/alchemist-original-imafzfhzefg8etxh.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/kjhgzgw0-0/book/m/m/u/alchemist-original-imafzfhzefg8etxh.jpeg?q=70",
         "price": 349,
-        "bookformat": ["Paperback", "Kindle", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["Paulo Coelho"],
         "language":["English", "Marathi"],
@@ -15,9 +15,9 @@ const productsList = [
 
     {
         "title": "Atomic Habits",
-        "image": "https://rukminim1.flixcart.com/image/612/612/ki4w0i80-0/book/l/t/u/atomic-habits-original-imafxzkkww75tpmm.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/ki4w0i80-0/book/l/t/u/atomic-habits-original-imafxzkkww75tpmm.jpeg?q=70",
         "price": 499,
-        "bookformat": ["Paperback", "Kindle", "Audio book"],
+        "book_format": ["Paperback", "Kindle", "Audio book"],
         "genre": "Self help",
         "author": ["James Clear"],
         "language":["English", "Marathi", "Hindi", "French"],
@@ -28,9 +28,9 @@ const productsList = [
 
     {
         "title": "Think like a Monk",
-        "image": "https://rukminim1.flixcart.com/image/612/612/kn0n6a80/regionalbooks/z/8/v/think-like-a-monk-by-jay-shetty-paperback-english-paperback-jay-original-imagfs94zhzhxzrc.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/kn0n6a80/regionalbooks/z/8/v/think-like-a-monk-by-jay-shetty-paperback-english-paperback-jay-original-imagfs94zhzhxzrc.jpeg?q=70",
         "price": 389,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook"],
         "genre": "Self help",
         "author": ["Jay Shetty"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -40,9 +40,9 @@ const productsList = [
     },
     {
         "title": "Harry Potter and the Philosopher's Stone",
-        "image": "https://rukminim1.flixcart.com/image/612/612/k6l2vm80/book/6/5/2/harry-potter-and-the-philosopher-s-stone-original-imaf4fwhjgnqzczv.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/k6l2vm80/book/6/5/2/harry-potter-and-the-philosopher-s-stone-original-imaf4fwhjgnqzczv.jpeg?q=70",
         "price": 500,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["J. K. Rowling"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -52,9 +52,9 @@ const productsList = [
     },
     {
         "title": "Harry Potter and the Chamber of Secrets",
-        "image": "https://rukminim1.flixcart.com/image/612/612/k2dm7bk0/book/6/6/9/harry-potter-and-the-chamber-of-secrets-original-imafhqpngjfyjezy.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/k2dm7bk0/book/6/6/9/harry-potter-and-the-chamber-of-secrets-original-imafhqpngjfyjezy.jpeg?q=70",
         "price": 500,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["J. K. Rowling"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -65,9 +65,9 @@ const productsList = [
 
     {
         "title": "Harry Potter and the Prisoner of Azkaban",
-        "image": "https://rukminim1.flixcart.com/image/612/612/k2dm7bk0/book/6/4/4/harry-potter-and-the-prisoner-of-azkaban-original-imafhqppnujx9kfg.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/k2dm7bk0/book/6/4/4/harry-potter-and-the-prisoner-of-azkaban-original-imafhqppnujx9kfg.jpeg?q=70",
         "price": 500,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["J. K. Rowling"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -78,9 +78,9 @@ const productsList = [
 
     {
         "title": "Harry Potter and the Goblet of Fire",
-        "image": "https://rukminim1.flixcart.com/image/612/612/k7ksvww0/book/6/8/3/harry-potter-and-the-goblet-of-fire-original-imafpsy3rfusxpwg.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/k7ksvww0/book/6/8/3/harry-potter-and-the-goblet-of-fire-original-imafpsy3rfusxpwg.jpeg?q=70",
         "price": 500,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["J. K. Rowling"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -91,9 +91,9 @@ const productsList = [
 
     {
         "title": "Harry Potter and the Order of the Phoenix",
-        "image": "https://rukminim1.flixcart.com/image/612/612/juk4gi80/book/6/9/0/harry-potter-and-the-order-of-the-phoenix-original-imaffngwextzpnpe.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/juk4gi80/book/6/9/0/harry-potter-and-the-order-of-the-phoenix-original-imaffngwextzpnpe.jpeg?q=70",
         "price": 500,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["J. K. Rowling"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -104,9 +104,9 @@ const productsList = [
 
     {
         "title": "Harry Potter and the Half-Blood Prince",
-        "image": "https://rukminim1.flixcart.com/image/612/612/juoes280/book/7/0/6/harry-potter-and-the-half-blood-prince-original-imaffzrgtjn82z9j.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/juoes280/book/7/0/6/harry-potter-and-the-half-blood-prince-original-imaffzrgtjn82z9j.jpeg?q=70",
         "price": 500,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["J. K. Rowling"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -117,9 +117,9 @@ const productsList = [
 
     {
         "title": "Harry Potter and the Deathly Hallows",
-        "image": "https://rukminim1.flixcart.com/image/612/612/kiow6fk0-0/book/v/p/8/harry-potter-and-the-deathly-hallows-original-imafyewy8dxmfszc.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/kiow6fk0-0/book/v/p/8/harry-potter-and-the-deathly-hallows-original-imafyewy8dxmfszc.jpeg?q=70",
         "price": 500,
-        "bookformat": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
+        "book_format": ["Paperback", "Kindle","Hardcover", "Audiobook", "Special Edition"],
         "genre": "Fiction",
         "author": ["J. K. Rowling"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -130,9 +130,9 @@ const productsList = [
 
     {
         "title": "The Fountainhead",
-        "image": "https://rukminim1.flixcart.com/image/612/612/k7qinbk0/book/9/0/8/the-fountainhead-original-imafpweg8n5wa7x5.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/k7qinbk0/book/9/0/8/the-fountainhead-original-imafpweg8n5wa7x5.jpeg?q=70",
         "price": 1499,
-        "bookformat": ["Paperback", "Hardcover"],
+        "book_format": ["Paperback", "Hardcover"],
         "genre": "Fiction",
         "author": ["Ayn Rand"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -143,9 +143,9 @@ const productsList = [
 
     {
         "title": "Thinking, Fast And Slow",
-        "image": "https://rukminim1.flixcart.com/image/612/612/kg2l47k0/regionalbooks/e/y/u/thinking-fast-and-slow-penguin-press-non-fiction-original-imafwe4pmbkyrznf.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/kg2l47k0/regionalbooks/e/y/u/thinking-fast-and-slow-penguin-press-non-fiction-original-imafwe4pmbkyrznf.jpeg?q=70",
         "price": 1499,
-        "bookformat": ["Paperback", "Hardcover"],
+        "book_format": ["Paperback", "Hardcover"],
         "genre": "Psychology",
         "author": ["Daniel Kahneman"],
         "language":["English", "Marathi", "Hindi", "Spanish"],
@@ -156,9 +156,9 @@ const productsList = [
 
     {
         "title": "Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming",
-        "image": "https://rukminim1.flixcart.com/image/612/612/k0plpjk0/book/5/0/9/eloquent-javascript-3rd-edition-original-imafk66767tp3yzw.jpeg?q=70",
+        "image_url": "https://rukminim1.flixcart.com/image/612/612/k0plpjk0/book/5/0/9/eloquent-javascript-3rd-edition-original-imafk66767tp3yzw.jpeg?q=70",
         "price": 1999,
-        "bookformat": ["Paperback"],
+        "book_format": ["Paperback"],
         "genre": "Programming",
         "author": ["Haverbeke Marijn"],
         "language":["English","Spanish"],
--- routes/products.router.js
+++ routes/products.router.js
@@ -4,7 +4,7 @@ const {Product} = require('../models/product.models')
 const extend =  require('lodash')
 const productsList = require('../data/productsList.data.js')
 
-
+Product.insertArray(productsList)
 
 router.route('/')
 .get(async(req, res)=>{
