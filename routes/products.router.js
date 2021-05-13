44251e51660191b1d3122887062b0e47a693e579
Renuka Paturkar <67357703+renukapaturkar@users.noreply.github.com>
2021-05-13 09:55:17 +0530

    feature: added the product id route in products

--- routes/products.router.js
+++ routes/products.router.js
@@ -30,4 +30,27 @@ router.route('/')
     }
 })
 
+
+router
+.route("/:productId")
+.get(async(req, res)=> {
+    try{
+        const {productId} = req.params;
+        const data = await Product.findById(productId).populate("productsArray.productId");
+        if(!data){
+            res
+            .status(400)
+            .json({success: false, message:"Could not found the product."})
+        }
+        res.json({success: true, data});
+
+    }catch(error){
+        res.status(500)
+        .json({
+            success: false, message: "Internal Server Error", errorMessage: error.message
+        });
+    }
+
+});
+
 module.exports = router
